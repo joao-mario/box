d5e6c739044c942a833a5aa6a70e93a587040174
joao mario <jjsp0403@gmail.com>
2020-03-02 20:00:16 -0300

    adicionando o projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("|   joao    |");
+            Console.WriteLine("+-----------+");
+        }
+    }
+}
